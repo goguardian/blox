bdfbbfe849faad75d5be38e48cd6e1406965a293
Igor Dubinskiy <igor@goguardian.com>
2017-11-20 18:52:36 -0800

    Get cluster-state-service build to pass

--- cluster-state-service/scripts/mockgen.sh
+++ cluster-state-service/scripts/mockgen.sh
@@ -39,7 +39,7 @@ cat << EOF
 // express or implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
-$(mockgen "${package}" "${interfaces}")
+$(mockgen -package mocks "${package}" "${interfaces}")
 EOF
 )
 
--- cluster-state-service/swagger/v1/generated/models/task_container_network_bindings.go
+++ cluster-state-service/swagger/v1/generated/models/task_container_network_bindings.go
@@ -19,50 +19,42 @@ package models
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
+	"strconv"
+
 	strfmt "github.com/go-openapi/strfmt"
 
 	"github.com/go-openapi/errors"
 	"github.com/go-openapi/swag"
 )
 
-// ContainerInstanceVersionInfo container instance version info
-// swagger:model ContainerInstanceVersionInfo
-type ContainerInstanceVersionInfo struct {
+// TaskContainerNetworkBindings task container network bindings
+// swagger:model taskContainerNetworkBindings
+type TaskContainerNetworkBindings []*TaskNetworkBinding
 
-	// agent hash
-	AgentHash string `json:"agentHash,omitempty"`
-
-	// agent version
-	AgentVersion string `json:"agentVersion,omitempty"`
-
-	// docker version
-	DockerVersion string `json:"dockerVersion,omitempty"`
-}
-
-// Validate validates this container instance version info
-func (m *ContainerInstanceVersionInfo) Validate(formats strfmt.Registry) error {
+// Validate validates this task container network bindings
+func (m TaskContainerNetworkBindings) Validate(formats strfmt.Registry) error {
 	var res []error
+
+	for i := 0; i < len(m); i++ {
+
+		if swag.IsZero(m[i]) { // not required
+			continue
+		}
+
+		if m[i] != nil {
+
+			if err := m[i].Validate(formats); err != nil {
+				if ve, ok := err.(*errors.Validation); ok {
+					return ve.ValidateName(strconv.Itoa(i))
+				}
+				return err
+			}
+		}
+
+	}
 
 	if len(res) > 0 {
 		return errors.CompositeValidationError(res...)
 	}
-	return nil
-}
-
-// MarshalBinary interface implementation
-func (m *ContainerInstanceVersionInfo) MarshalBinary() ([]byte, error) {
-	if m == nil {
-		return nil, nil
-	}
-	return swag.WriteJSON(m)
-}
-
-// UnmarshalBinary interface implementation
-func (m *ContainerInstanceVersionInfo) UnmarshalBinary(b []byte) error {
-	var res ContainerInstanceVersionInfo
-	if err := swag.ReadJSON(b, &res); err != nil {
-		return err
-	}
-	*m = res
 	return nil
 }
